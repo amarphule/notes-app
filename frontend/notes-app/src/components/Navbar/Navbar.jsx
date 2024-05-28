4b3abf13cb2197281d39649c5296da7328c75670
amarphule <amardeepphule@gmail.com>
2024-05-28 18:37:52 +0530

    add searchbar

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ProfileInfo from "../Cards/ProfileInfo";
+import SearchBar from "../SearchBar/SearchBar";
+
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState();
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
+  const handleSearch = () => {};
   return (
     <div className="bg-white drop-shadow flex px-8 py-2 items-center justify-between">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onClearSearch={onClearSearch}
+        handleSearch={handleSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
